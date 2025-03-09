de19fa18e1c4a3149dd019f7448ddf997fbc0859
motusira <torusama@yandex.ru>
2025-03-10 02:33:32 +0300

    added font size scaling

--- src/ui/ui.cpp
+++ src/ui/ui.cpp
@@ -12,9 +12,9 @@ void UI::update(const Vector2 &window_size) {
 
 static void draw_slide_bar(const float x, const float y, const float width,
                            const float height, float &val, const char *text,
-                           const float min, const float max) {
+                           const float min, const float max, const int font_size) {
   GuiSliderBar((Rectangle){x, y, width, height}, NULL, NULL, &val, min, max);
-  DrawText(TextFormat(text, val), x + 10, y + height + 10, 20, BLACK);
+  DrawText(TextFormat(text, val), x + 10, y + height + 10, font_size, BLACK);
 }
 
 void UI::draw(const Vector2 &window_size, Grid &g) {
@@ -23,31 +23,29 @@ void UI::draw(const Vector2 &window_size, Grid &g) {
   float y = viewport.y + 10;
   float width = viewport.width / 3 * 2;
   float height = viewport.height / 20;
+  int font_size = height / 3 * 2;
 
   BeginScissorMode(viewport.x, viewport.y, viewport.width, viewport.height);
 
   DrawRectangle(viewport.x, viewport.y, viewport.width, viewport.height,
                 LIGHTGRAY);
-  DrawFPS(x, y);
-
-  y += 30;
 
   float s = g.get_cell_size();
-  draw_slide_bar(x, y, width, height, s, "Cell size: %.0f", 1, 64);
+  draw_slide_bar(x, y, width, height, s, "Cell size: %.0f", 1, 64, font_size);
   y += 3 * height;
   if (g.get_cell_size() != (int)s) {
     g.set_cell_size((int)s);
   }
 
   float d = g.get_grid_size();
-  draw_slide_bar(x, y, width, height, d, "Grid size: %.0f", 1, 256);
+  draw_slide_bar(x, y, width, height, d, "Grid size: %.0f", 1, 256, font_size);
   y += 3 * height;
   if (g.get_grid_size() != (int)d) {
     g.set_grid_size((int)d);
   }
 
   float u = g.get_update_interval();
-  draw_slide_bar(x, y, width, height, u, "Update interval: %.2f", 0.01, 0.5);
+  draw_slide_bar(x, y, width, height, u, "Update interval: %.2f", 0.01, 0.5, font_size);
   y += 3 * height;
   if (g.get_update_interval() != u) {
     g.set_update_interval(u);
